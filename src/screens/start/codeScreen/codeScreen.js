043f6ab2cc91fe152a5990d4391c0d0ef514d6a9
vanGoghhh <blackznigelng@gmail.com>
2021-01-08 16:36:31 +0800

    create code screen

--- src/screens/start/codeScreen/codeScreen.js
+++ src/screens/start/codeScreen/codeScreen.js
@@ -1,10 +1,37 @@
 import React from "react";
-import "./codeScreen.css";
+import "./codeScreen.scss";
 
+const CodeScreen = () => {
+	const [gameCode, setGameCode] = useState("");
 
-const codeScreen = () => {
-    return (
-        <div>
-        </div>
-    )
-}
+	// send userName to server
+	const handleSubmit = () => {};
+	return (
+		<div className="tb-code-screen__container">
+			<div className="tb-code-screen__container__input-box">
+				<div className="tb-code-screen__container__input-box__title">
+					Enter username
+				</div>
+				<form className="tb-code-screen__container__input-box__input">
+					<input
+						onChange={(e) => setUserName(e.target.value)}
+						placeholder="Enter Name"
+						className="tb-code-screen__container__input-box__input__textfield"
+					/>
+					<button
+						type="submit"
+						onClick={handleSubmit}
+						className="tb-code-screen__container__input-box__input__submit-button"
+					>
+						Enter Name
+					</button>
+					<button className="tb-code-screen__container__input-box__input__back-button">
+						Back
+					</button>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+export default CodeScreen
--- src/screens/start/nameScreen/nameScreen.js
+++ src/screens/start/nameScreen/nameScreen.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import './nameScreen.css';
+import React, { useState } from "react";
+import "./nameScreen.scss";
 
 const NameScreen = () => {
-	const [userName, setUserName] = useState('');
+	const [userName, setUserName] = useState("");
 
 	// send userName to server
 	const handleSubmit = () => {};
@@ -16,9 +16,18 @@ const NameScreen = () => {
 					<input
 						onChange={(e) => setUserName(e.target.value)}
 						placeholder="Enter Name"
-					></input>
-					<button type="submit" onClick={handleSubmit}></button>
-					<button>Back</button>
+						className="tb-name-screen__container__input-box__input__textfield"
+					/>
+					<button
+						type="submit"
+						onClick={handleSubmit}
+						className="tb-name-screen__container__input-box__input__submit-button"
+					>
+						Enter Name
+					</button>
+					<button className="tb-name-screen__container__input-box__input__back-button">
+						Back
+					</button>
 				</form>
 			</div>
 		</div>
